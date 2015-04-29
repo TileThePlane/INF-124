a193b65d2705df7ef79c9dd6d32a872a5c2652cd
Morphaz <kvonderw@uci.edu>
2015-04-29 15:28:32 -0700

    Quote script working
    Still need to do the arbitrary maths

--- scripts/sockQuote.js
+++ scripts/sockQuote.js
@@ -1,12 +1,9 @@
 function printQuote(){
   var sockQuote = document.getElementById("sockQuote");
-  var text = "";
-  var i;
-  for (i = 0; i < x.length ;i++) {
-      text += x.elements[i].value + "<br>";
-  }
-  document.getElementById("demo").innerHTML = text;
+  document.getElementById("printSockQuote").innerHTML = sockQuote.splice(3,1).value;
   var sockQuantity = sockQuote.elements[0].value;
   var sockColor = sockQuote.elements[1].value;
   var sockPattern = sockQuote.elements[2].value;
+  }
+
 }
