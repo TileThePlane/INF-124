d5d5e0b5d5983842f70580f5eb3010fab843335a
Morphaz <kvonderw@uci.edu>
2015-04-29 15:28:35 -0700

    Revert "Quote script working"
    This reverts commit a193b65d2705df7ef79c9dd6d32a872a5c2652cd.

--- scripts/sockQuote.js
+++ scripts/sockQuote.js
@@ -1,9 +1,12 @@
 function printQuote(){
   var sockQuote = document.getElementById("sockQuote");
-  document.getElementById("printSockQuote").innerHTML = sockQuote.splice(3,1).value;
+  var text = "";
+  var i;
+  for (i = 0; i < x.length ;i++) {
+      text += x.elements[i].value + "<br>";
+  }
+  document.getElementById("demo").innerHTML = text;
   var sockQuantity = sockQuote.elements[0].value;
   var sockColor = sockQuote.elements[1].value;
   var sockPattern = sockQuote.elements[2].value;
-  }
-
 }
