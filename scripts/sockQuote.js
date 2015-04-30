02e7d9112fe1f672cc8dd57054d4c2ad5c2e3697
Morphaz <kvonderw@uci.edu>
2015-04-29 17:57:36 -0700

    Fully functioning sockQuote App
    App in buy.html

--- scripts/sockQuote.js
+++ scripts/sockQuote.js
@@ -1,12 +1,35 @@
 function printQuote(){
   var sockQuote = document.getElementById("sockQuote");
-  var text = "";
-  var i;
-  for (i = 0; i < x.length ;i++) {
-      text += x.elements[i].value + "<br>";
+  var sockQuantity = sockQuote.sockQuantity.value;
+  var sockColor = sockQuote.sockColor.value;
+  var sockPattern = sockQuote.sockPattern.value;
+
+  var quote = 1000;
+  if(sockColor === '#000000' || sockColor === '#ffffff'){
+    quote += 1;
+  } else {
+    quote += 2;
   }
-  document.getElementById("demo").innerHTML = text;
-  var sockQuantity = sockQuote.elements[0].value;
-  var sockColor = sockQuote.elements[1].value;
-  var sockPattern = sockQuote.elements[2].value;
+
+  switch(sockPattern){
+    case 'argyle':
+      quote += 2;
+      break;
+    case 'checkered':
+      quote += 3;
+      break;
+    case 'striped':
+      quote += 4;
+      break;
+    case 'solid':
+      quote += 1;
+      break;
+    case 'alpaca':
+      quote += 20;
+      break;
+  }
+
+  quote = quote*sockQuantity;
+
+  document.getElementById("printSockQuote").innerHTML = "Our estimate is: " + quote;
 }
