418899ed75edc83bd5db6193301f8186db94cb13
Morphaz <kvonderw@uci.edu>
2015-05-04 12:43:26 -0700

    fixed rotating socks in buy

--- scripts/rotatingSockImages.js
+++ scripts/rotatingSockImages.js
@@ -1,16 +1,17 @@
-dir = 'image/rotate'
+var dir = 'images/rotate/'
 var imageArray = new Array(7);
 for (i=0; i<imageArray.length; i++)
   imageArray[i] = dir + (i+1) + '.jpg';
 
-var imgCounter = 0;
-function rotateImage{
+var imageCounter = 0;
+function rotateImage(){
   var image = document.getElementById('placeHolder');
   image.src = imageArray[imageCounter];
   ++imageCounter;
-  if (imageCounter == 6) imageCounter = 0;
+  if (imageCounter == 7) imageCounter = 0;
 }
 
-function getRotatingSockImages();
+function getRotatingSockImages(){
   document.getElementById('placeHolder').src=imageArray[5];
   setInterval('rotateImage()', 2000);
+}
