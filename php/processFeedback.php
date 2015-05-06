7c79897559797c013c46f6de6bd85902e2fdd8b1
Andy <aschlali@uci.edu>
2015-05-06 03:16:14 -0700

    Fixed some problems in processfeedback

--- php/processFeedback.php
+++ php/processFeedback.php
@@ -12,22 +12,22 @@ $messageToBusiness =
 //Construct the confirmation message to be e-mailed to the client
 $messageToClient =
     "Dear ".$_POST['salute']." ".$_POST['lastName'].":\r\n".
-    "The following message was received from you by Nature's Source:\r\n\r\n".
+    "The following message was received from you by Sock Exchange:\r\n\r\n".
     $messageToBusiness.
-    "------------------------\r\nThank you for the feedback and your patronage.\r\n" .
-    "The Nature's Source Team\r\n------------------------\r\n";
+    "------------------------\r\nThank you for your Feedback :D\r\n" .
+    "The Sock Exchange Team\r\n------------------------\r\n";
 if ($_POST['mail list'])
     $messageToClient .= "P.S. We will add you to our mail list!";
 else
 	$messageToClient .= "P.S. We will not add you to our mail list. If you would like to be added, let us know!";
 //Send the confirmation message to the client
-$headerToClient = "From: webbook@cs.smu.ca\r\n";
+$headerToClient = "From: kvonderw@uci.edu\r\n";
 mail($_POST['email'], "Re: ".$_POST['subject'], $messageToClient, $headerToClient);
 
 //Transform the confirmation message to the client into XHTML format and display it
 $display = str_replace("\r\n", "<br />\r\n", $messageToClient);
 $display =
-    "<html><head><title>Your Message was recieved! Thank you for your Feedback :D</title></head><body><tt>".
+    "<html><head><title>Feedback</title></head><body><tt>".
     $display.
     "</tt></body></html>";
 echo $display;
